c30cc274b1ec96ad9ecbdf5ecb2e73632415ae39
HYec <i@hyec.me>
2017-10-13 22:28:49 +0800

    add admin room info

--- src/main/java/project/hms/controller/RoomController.java
+++ src/main/java/project/hms/controller/RoomController.java
@@ -5,7 +5,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import project.hms.model.Room;
 import project.hms.repository.RoomRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin/room")
@@ -23,4 +27,18 @@ public class RoomController {
         model.addAttribute("rooms", repository.findAll());
         return "room/list";
     }
+
+    @GetMapping("/info")
+    public String info(@RequestParam("id") Integer id,
+                       Model model) throws Exception {
+
+        Optional<Room> roomOptional = repository.findById(id);
+        if (!roomOptional.isPresent()) {
+            throw new Exception("Error");
+        }
+
+        model.addAttribute("room", roomOptional.get());
+        return "room/info";
+    }
+
 }
--- src/main/java/project/hms/repository/RoomRepository.java
+++ src/main/java/project/hms/repository/RoomRepository.java
@@ -6,8 +6,11 @@ import project.hms.model.enums.RoomStatus;
 import project.hms.model.enums.RoomType;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RoomRepository extends JpaRepository<Room, Integer> {
+
+    Optional<Room> findById(int id);
 
     List<Room> findAllByType(RoomType type);
 
