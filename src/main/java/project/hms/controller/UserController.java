f77f556d0c11912c6c951b965ed36d96ed72de5e
HYec <i@hyec.me>
2017-10-17 21:19:08 +0800

    add register to UserController.

--- src/main/java/project/hms/controller/UserController.java
+++ src/main/java/project/hms/controller/UserController.java
@@ -3,12 +3,17 @@ package project.hms.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import project.hms.data.dto.UserDto;
 import project.hms.model.User;
 import project.hms.repository.UserRepository;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @Controller
@@ -32,6 +37,21 @@ public class UserController {
     @GetMapping("/login")
     public String login() {
         return "login";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerPOST(
+            @Valid UserDto userDto,
+            BindingResult result
+    ) {
+        return "redirect:/user/login";
     }
 
 }
