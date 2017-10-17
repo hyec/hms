3bee731538be6bb9109660a7c6e69d667dbc19a6
HYec <i@hyec.me>
2017-10-17 21:09:12 +0800

    add OrderRepository#findAllByTime and OrderService

--- src/main/java/project/hms/repository/OrderRepository.java
+++ src/main/java/project/hms/repository/OrderRepository.java
@@ -1,11 +1,13 @@
 package project.hms.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import project.hms.model.Order;
 import project.hms.model.Room;
 import project.hms.model.User;
 import project.hms.model.enums.OrderStatus;
 
+import java.util.Date;
 import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Integer> {
@@ -15,4 +17,9 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
     List<Order> findAllByRoom(Room room);
 
     List<Order> findAllByStatus(OrderStatus orderStatus);
+
+    @Query("select o from Order o " +
+            "where o.checkInTime >= #{startTime} and o.checkInTime <= #{endTime} " +
+            "or o.checkOutTime >= #{startTime} and o.checkOutTime <= #{endTime} ")
+    List<Order> findAllByTime(Date startTime, Date endTime);
 }
---
+++ src/main/java/project/hms/service/OrderService.java
@@ -0,0 +1,56 @@
+package project.hms.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.hms.model.Order;
+import project.hms.model.Room;
+import project.hms.model.enums.OrderStatus;
+import project.hms.model.enums.RoomType;
+import project.hms.repository.OrderRepository;
+import project.hms.repository.RoomRepository;
+
+import java.util.*;
+
+@Service
+public class OrderService {
+
+    private final RoomRepository roomRepository;
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(RoomRepository roomRepository, OrderRepository orderRepository) {
+        this.roomRepository = roomRepository;
+        this.orderRepository = orderRepository;
+    }
+
+    public List<Room> getAvailableRooms(RoomType type, Date start, Date end) {
+        Calendar cal = Calendar.getInstance();
+
+        cal.setTime(start);
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        start = cal.getTime();
+
+        cal.setTime(end);
+        cal.set(Calendar.HOUR_OF_DAY, 23);
+        cal.set(Calendar.MINUTE, 59);
+        cal.set(Calendar.SECOND, 59);
+        end = cal.getTime();
+
+        List<Room> rooms = roomRepository.findAllByType(type);
+        List<Order> orders = orderRepository.findAllByTime(start, end);
+
+        Set<Room> unavailRooms = new HashSet<>();
+
+        orders.forEach((order -> {
+            if (order.getStatus() != OrderStatus.CANCEL)
+                unavailRooms.add(order.getRoom());
+        }));
+        rooms.removeAll(unavailRooms);
+
+        return rooms;
+    }
+
+}
