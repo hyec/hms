b57b26bea2d0d45968fdf9ed651633be33aa32fe
XH1 <514587459@qq.com>
2017-10-24 21:29:41 +0800

    ok

--- src/main/java/project/hms/HMSApplication.java
+++ src/main/java/project/hms/HMSApplication.java
@@ -2,10 +2,11 @@ package project.hms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HMSApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HMSApplication.class, args);
 	}
--- src/main/java/project/hms/controller/AdminOrderController.java
+++ src/main/java/project/hms/controller/AdminOrderController.java
@@ -1,16 +1,22 @@
 package project.hms.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import project.hms.model.Order;
+import project.hms.model.User;
 import project.hms.repository.OrderRepository;
 import project.hms.repository.RoomRepository;
+import project.hms.repository.UserRepository;
+import project.hms.util.ModelTool;
 
 import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 
 @Controller
@@ -18,17 +24,19 @@ import java.util.Optional;
 public class AdminOrderController {
 
     private final OrderRepository orderRepository;
+    private final UserRepository userRepository;
     // private final RoomRepository roomRepository;
+
+    @Autowired
+    public AdminOrderController(OrderRepository orderRepository, RoomRepository roomRepository, UserRepository userRepository) {
+        this.orderRepository = orderRepository;
+        //this.roomRepository = roomRepository;
+        this.userRepository = userRepository;
+    }
 
     @GetMapping("/")
     public String index() {
         return "redirect:/admin/order/list";
-    }
-
-    @Autowired
-    public AdminOrderController(OrderRepository orderRepository, RoomRepository roomRepository) {
-        this.orderRepository = orderRepository;
-        //this.roomRepository = roomRepository;
     }
 
     @GetMapping("/list")
@@ -36,6 +44,7 @@ public class AdminOrderController {
         model.addAttribute("orders", orderRepository.findAll());
         return "order/list";
     }
+
 
     @GetMapping("/info")
     public String info(@RequestParam("id") Integer id,
@@ -50,13 +59,36 @@ public class AdminOrderController {
         return "order/info";
     }
 
+    @PostMapping("/edit/user")
+    public String editUser(@RequestParam(value = "user", required = false) List<Integer> staysId, @RequestParam("orderId") Integer orderId) throws Exception {
+        Optional<Order> orderOptional = orderRepository.findById(orderId);
+        if (!orderOptional.isPresent()) {
+            throw new Exception("wrong orderId");
+        }
+        Order order = orderOptional.get();
+        if (staysId != null && staysId.size() > 0) {
+            Set<User> set = new HashSet<>();
+            for (int stayId : staysId) {
+                Optional<User> userOptional = userRepository.findById(stayId);
+                if (!userOptional.isPresent()) {
+                    throw new Exception("wrong userId");
+                }
+                set.add(userOptional.get());
+            }
+            set.add(order.getOwner());
+            order.setStays(set);
+            orderRepository.save(order);
+        }
+        return "redirect:/admin/order/edit?id=" + orderId;
+    }
+
     @GetMapping("/edit")
     public String edit(@RequestParam(value = "id") Integer id,
                        Model model) throws Exception {
 
         Optional<Order> orderOptional = orderRepository.findById(id);
         if (!orderOptional.isPresent()) {
-            throw new Exception("Error");
+            throw new Exception("wrong id");
         }
 
         model.addAttribute("order", orderOptional.get());
@@ -65,9 +97,18 @@ public class AdminOrderController {
     }
 
     @PostMapping("/edit")
-    public String editPOST(@ModelAttribute("order") @Valid Order order, BindResult result) {
+    public String editPOST(@ModelAttribute("order") @Valid Order order, BindingResult result) throws Exception {
+        if (result.hasErrors()) {
+            throw new Exception("Error");
+        }
+        final Optional<Order> oldOrderOptional = orderRepository.findById(order.getId());
+        if (!oldOrderOptional.isPresent()) {
+            throw new Exception("err");
+        }
+        Order oldOrder = oldOrderOptional.get();
 
-        Order savaOrder = orderRepository.save(order);
-        return "redirect:/admin/order/info?id=" + savaOrder.getId();
+        ModelTool.merge(order, oldOrder);
+        order = orderRepository.save(oldOrder);
+        return "redirect:/admin/order/info?id=" + order.getId();
     }
 }
